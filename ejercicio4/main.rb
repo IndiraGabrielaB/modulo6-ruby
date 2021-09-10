d82e99bdeed7332a5ef8b96844a4041917958a00
indira <indiragabrielab@hotmail.com>
2021-09-09 23:36:41 -0300

    ejercicios-modulos

--- ejercicio4/main.rb
+++ ejercicio4/main.rb
@@ -1,4 +1,9 @@
-require_relative 'modulo_numeros'
-include ModuloNumeros
+require_relative 'modulo_usuarios'          # enlazar los archivos  modulo_usuarios.rb
+require_relative 'modulo_impresiones'       # enlaza los archivos  modulo_impresiones.rb
 
-mostras_menu
+include ModuloUsuarios                      # incluye el modulo usuarios
+include ModuloImpresiones                   # incluye el modulo impresiones
+
+arreglo_usuarios_correo_faker = generar_usuario_con_correo(5)       # modulo usuarios
+
+imprimir_usuarios_con_correos(arreglo_usuarios_correo_faker)        # modulo impresiones
